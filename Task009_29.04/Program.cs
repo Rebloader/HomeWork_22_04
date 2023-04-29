9b174f12b6983dd330135baab969a4d3ac10e927
Rebloader <12mrdroid08@gmail.com>
2023-04-29 18:48:45 +0300

    Add homework of Seminar3 29.04.23

---
+++ Task009_29.04/Program.cs
@@ -0,0 +1,24 @@
+﻿// Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом.
+// 14212 -> нет
+// 12821 -> да
+// 23432 -> да
+
+
+System.Console.WriteLine("input number: ");
+string? str = Console.ReadLine();
+
+if (str.Length == 5)
+{
+    if (str[0] == str[4] && str[1] == str[3])
+    {
+    System.Console.WriteLine("True");
+    }
+    else
+    {
+    System.Console.WriteLine("False");
+    }
+}
+else
+{
+    System.Console.WriteLine("Error");
+}
---
+++ Task010_29.04/Program.cs
@@ -0,0 +1,21 @@
+﻿// Напишите программу, которая принимает на вход координаты двух точек и находит расстояние между ними в 3D пространстве.
+// A (3,6,8); B (2,1,-7), -> 15.84
+// A (7,-5, 0); B (1,-1,9) -> 11.53
+
+int InputNum(string text)
+{
+    System.Console.Write(text);
+    return Convert.ToInt32(Console.ReadLine());
+}
+int[] coordsA = new int[3];
+coordsA[0] = InputNum("Input X coords point A: ");
+coordsA[1] = InputNum("Input Y coords point A: ");
+coordsA[2] = InputNum("Input Z coords point A: ");
+
+int[] coordsB = new int[3];
+coordsB[0] = InputNum("Input X coords point B: ");
+coordsB[1] = InputNum("Input Y coords point B: ");
+coordsB[2] = InputNum("Input Z coords point B: ");
+
+double distance = Math.Sqrt(Math.Pow(coordsA[0] - coordsB[0], 2) + Math.Pow(coordsA[1] - coordsB[1], 2) + Math.Pow(coordsA[2] - coordsB[2], 2));
+System.Console.WriteLine(Math.Round(distance, 2));
