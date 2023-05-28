def28dd16fc131fddbe2f5ac25a0b9876b06889c
Rebloader <12mrdroid08@gmail.com>
2023-05-28 14:52:23 +0300

    Add homework 28.05.23

---
+++ Task_05.28/Task019_28.05/Program.cs
@@ -0,0 +1,47 @@
+﻿// Задайте двумерный массив размером m×n, заполненный случайными вещественными числами.
+// m = 3, n = 4.
+// 0,5 7 -2 -0,2
+// 1 -3,3 8 -9,9
+// 8 7,8 -7,1 9
+
+
+void Main()
+{
+    double[,] matrix = GenerateMatrix(ReadInt("Input rows: "), ReadInt("Input columns: "), -10, 10);
+    PrintMatrix(matrix);
+    
+}
+
+int ReadInt(string text)
+{
+    System.Console.Write(text);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+double[,] GenerateMatrix(int row, int col, double minNum, double maxNum)
+{
+    double[,] matrix = new double[row, col];
+    Random rand = new Random();
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = Math.Round((rand.NextDouble()*(maxNum - minNum) + minNum), 2);
+        }
+    }
+    return matrix;
+}
+
+void PrintMatrix(double[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            System.Console.Write(matrix[i, j] + "\t ");
+        }
+        System.Console.WriteLine();
+    }
+}
+
+Main();
---
+++ Task_05.28/Task020_28.05/Program.cs
@@ -0,0 +1,56 @@
+﻿// Задача 50. Напишите программу, которая на вход принимает позиции элемента в двумерном массиве, 
+// и возвращает значение этого элемента или же указание, что такого элемента нет.
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// 17 -> такого числа в массиве нет
+
+void Main()
+{
+    int[,] matrix = GenerateMatrix(ReadInt("Input rows: "), ReadInt("Input columns: "), 0, 50);
+    PrintMatrix(matrix);
+    int number = FindElement(matrix, ReadInt("Input rows №: "), ReadInt("Input columns №: "));
+    if (number != -1) System.Console.WriteLine(number);
+    else System.Console.WriteLine("No number in matrix");
+    
+}
+int ReadInt(string text)
+{
+    System.Console.Write(text);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+int[,] GenerateMatrix(int row, int col, int minNum, int maxNum)
+{
+    int[,] matrix = new int[row,col];
+    Random rand = new Random();
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i,j] = rand.Next(minNum, maxNum + 1);
+        }
+    }
+    return matrix;
+}
+
+void PrintMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            System.Console.Write(matrix[i, j] + "\t ");
+        }
+        System.Console.WriteLine();
+    }
+}
+
+int FindElement(int[,] matrix, int row, int col)
+{ 
+    if(row < matrix.GetLength(0) && col < matrix.GetLength(1)) return matrix[row-1, col-1];
+    return -1;
+}
+
+Main();
